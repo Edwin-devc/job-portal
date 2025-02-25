0ca2ec9794a5e5258d96e35ddd36c8ec3cf13ee6
Rwakasiisi Edwin <erwakasiisi@gmail.com>
2025-02-25 10:04:24 +0300

    ref:Remove repeated code

--- routes/api.php
+++ routes/api.php
@@ -11,20 +11,25 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// JobPosting routes
-Route::get('/job-postings', [JobPostingController::class, 'index'])->middleware('auth:sanctum');
-Route::post('/job-postings', [JobPostingController::class, 'store'])->middleware('auth:sanctum');
-Route::get('/job-postings/{jobPosting}', [JobPostingController::class, 'show'])->middleware('auth:sanctum');
-Route::put('/job-postings/{jobPosting}', [JobPostingController::class, 'update'])->middleware('auth:sanctum');
-Route::delete('/job-postings/{jobPosting}', [JobPostingController::class, 'destroy'])->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    // JobPosting routes
+    Route::get('/job-postings', [JobPostingController::class, 'index']);
+    Route::post('/job-postings', [JobPostingController::class, 'store']);
+    Route::get('/job-postings/{jobPosting}', [JobPostingController::class, 'show']);
+    Route::put('/job-postings/{jobPosting}', [JobPostingController::class, 'update']);
+    Route::delete('/job-postings/{jobPosting}', [JobPostingController::class, 'destroy']);
 
-// JobApplication routes
-Route::get('/job-applications', [JobApplicationController::class, 'index'])->middleware('auth:sanctum');
-Route::post('/job-applications', [JobApplicationController::class, 'store'])->middleware('auth:sanctum');
-Route::get('/job-applications/{jobApplication}', [JobApplicationController::class, 'show'])->middleware('auth:sanctum');
-Route::put('/job-applications/{jobApplication}', [JobApplicationController::class, 'update'])->middleware('auth:sanctum');
-Route::delete('/job-applications/{jobApplication}', [JobApplicationController::class, 'destroy'])->middleware('auth:sanctum');
+    // JobApplication routes
+    Route::get('/job-applications', [JobApplicationController::class, 'index']);
+    Route::post('/job-applications', [JobApplicationController::class, 'store']);
+    Route::get('/job-applications/{jobApplication}', [JobApplicationController::class, 'show']);
+    Route::put('/job-applications/{jobApplication}', [JobApplicationController::class, 'update']);
+    Route::delete('/job-applications/{jobApplication}', [JobApplicationController::class, 'destroy']);
+
+    // Logout route
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
